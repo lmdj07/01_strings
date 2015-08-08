2d1e5a7b00b1e1d6356ee0ed2d335d22052fab1d
lmdj07 <lmdj07@gmail.com>
2015-08-08 23:24:08 +0100

    changed arrays to hash

---
+++ options.rb
@@ -0,0 +1,26 @@
+#first_attempt_at_coding
+
+$Opening_Balance = 1000
+$New_House = 650
+$Red_Ferrari = 225
+$Buy_Tech_Firm = 150
+$Worldwide_Cruise = 75
+$Pay_Off_Mortgage = 250
+$Visit_Las_Vegas = 100
+$Six_Months_off_Work = 50
+
+puts "what would you do with a million pounds?"
+#puts "your balance is x spend some more"
+options = {"New House" => 650,"Red Ferrari" => 225, "Buy Tech Firm" => 150, "Worldwide Cruise" => 75, "Pay off Mortgage" => 250, "Visit Las Vegas" => 100, "Six Months off Work" => 50}
+puts
+#show user list of options
+begin
+puts "type 'opt' for options"
+user_input = gets.strip
+end until user_input == 'opt'
+puts
+puts "Your choices & associated costs are:"
+puts
+puts options.each { |key, value| puts "#{key} - #{value}" }
+puts
+puts "What will it be?"
