8b9f39ce88721b3130866af060bd8a5d8acea856
lmdj07 <lmdj07@gmail.com>
2015-08-16 13:34:14 +0100

    changed hash to array

--- million_dollar_options.rb
+++ million_dollar_options.rb
@@ -1,28 +1,14 @@
 #first_attempt_at_coding
 
-#class Allowance
-
-  #def initialize ()
-    #@balance = 1000
-    #puts "Your opening balance is #{@balance}"
-    #end
-    #end
-
-$Opening_Balance = 1000
-$New_House = 650
-$Red_Ferrari = 225
-$Buy_Tech_Firm = 150
-$Worldwide_Cruise = 75
-$Pay_Off_Mortgage = 250
-$Visit_Las_Vegas = 100
-$Six_Months_Off_Work = 50
- 
 
 puts "what would you do with a million pounds?"
-#puts "your balance is x spend some more"
-options = {"New House" => 650,"Red Ferrari" => 225, "Buy Tech-Firm" => 150, "Worldwide Cruise" => 75, "Pay Off Mortgage" => 250, "Visit Las Vegas" => 100, "Six Months No-Work" => 50}
+
+options = [["New House",650],["Red Ferrari",225], ["Buy Tech-Firm",150], ["Worldwide Cruise",75], 
+["Pay Off Mortgage",250], ["Visit Las Vegas",100], ["Six Months No-Work",50]]
 puts
+
 #show user list of options
+
 begin
 puts "type 'opt' for options"
 user_input = gets.strip
@@ -30,40 +16,68 @@ end until user_input == 'opt'
 puts
 puts "Your choices are:"
 puts
-options.each { |key, value| puts "#{key}" }
+options.each { |key, value| puts "#{key}"}
 puts
 puts "What will you spend on?
-.. You have a choice of luxury items 
+.. You have a choice of an item\(s\) 
        up to a million pounds "
 puts 
 puts "type the last word of your choice 
 e.g \"Ferrari\" or \"House\" etc... \(case sensitive\)"
 puts
-user_choice = gets.chomp
+#user_choice = gets.chomp
+$Opening_Balance = 1000
+balance = $Opening_Balance - user_choice.to_i
 
-if user_choice == "Ferrari"
-puts "Your balance is #{$Opening_Balance - $Red_Ferrari}"
 
-elsif user_choice == "House"
-puts "Your balance is #{$Opening_Balance - $New_House}"
+#def choices ()
 
-elsif user_choice == "Tech-Firm"
-puts "Your balance is #{$Opening_Balance - $Buy_Tech_Firm}"
+if
+user_choice == "House"
+puts "Your balance is #{balance - options[0][1].to_i}"
 
-elsif user_choice == "Cruise"
-puts "Your balance is #{$Opening_Balance - $Worldwide_Cruise}"
+elsif
 
-elsif user_choice == "Mortgage"
-puts "Your balance is #{$Opening_Balance - $Pay_Off_Mortgage}"
+user_choice == "Ferrari"
+puts "Your balance is #{balance - options[1][1].to_i}"
 
-elsif user_choice == "Vegas"
-puts "Your balance is #{$Opening_Balance - $Visit_Las_Vegas}"
+elsif
 
-elsif user_choice == "No-Work"
-puts "Your balance is #{$Opening_Balance - $Six_Months_Off_Work}"
+user_choice == "Tech-Firm"
+puts "Your balance is #{balance - options[2][1].to_i}"
 
-else
-puts "Keep Shopping"
+elsif
+
+user_choice == "Cruise"
+puts "Your balance is #{balance - options[3][1].to_i}"
+
+elsif
+
+user_choice == "Mortgage"
+puts "Your balance is #{balance - options[4][1].to_i}"
+
+elsif
+
+user_choice == "Vegas"
+puts "Your balance is #{balance - options[5][1].to_i}"
+
+elsif
+
+user_choice == "No-Work"
+puts "Your balance is #{balance - options[6][1].to_i}"
+
+return user_choice
+
 end
+
+#while balance != 0 
+
+puts "continue?"
+end
+end
+
+#else
+#puts "Keep Shopping"
+#end
 
 
