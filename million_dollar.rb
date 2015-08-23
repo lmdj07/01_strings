c54c1614adad9ddbf1e04f1fa5b65ce2b58d3e21
lmdj07 <lmdj07@gmail.com>
2015-08-23 21:10:18 +0100

    tidied up a bit

--- million_dollar.rb
+++ million_dollar.rb
@@ -3,9 +3,9 @@ def display_balance(balance)
 end
 
 def display_menu(menu)
-  puts "***MENU***"
+  puts "*******************"
   menu.each { |key, value| puts "#{key}"}
-  
+  puts "*******************"
 end
 
 puts "what would you do with a million pounds?"
@@ -18,39 +18,41 @@ puts
 #show user list of options
 
 begin
-puts "type 'opt' for options"
-user_input = gets.strip
+  puts "type 'opt' for options"
+  user_input = gets.strip
 end until user_input == 'opt'
 puts
 puts "Your choices are:"
 puts
 display_menu(options)
 puts
-puts "What will you spend on?
-.. You have a choice of an item\(s\) 
-       up to a million pounds "
+puts "What will you spend on your million on?"
+
 
 balance = 1000
 
 begin 
 
-	puts 
-	puts "type the last word of your choice 
-	e.g \"Ferrari\" or \"House\" etc... \(case sensitive\)"
 	puts
 	user_choice = gets.chomp
   	 
 	if  options.has_key?(user_choice)
 		balance = balance - options[user_choice]
 		display_balance (balance)
+		puts "keep spending ?"
 		
 	else 
-		puts "invalid choice"
+		puts "<invalid choice>"
+		puts
 		display_menu(options)
+		puts "only above list (case sensitive)"
 	end
 
 
 end while balance >0
 
+#IDEAS TO ENHANCE GAME
+#IF USER DOES NOT WANT TO USE ALL THEY CAN GIVE TO CHARITY OF THEIR CHOICE
+#IF USER GOES OVER A MILLION, THEY NEED TO SELL BACK TO THE GAME AT A 5% LOSS
 
 
