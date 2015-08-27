3c1cd7f5bfdb679fbadcad75fb406fd780375cde
lmdj07 <lmdj07@gmail.com>
2015-08-28 00:28:22 +0100

    added overspend warning

--- million_dollar.rb
+++ million_dollar.rb
@@ -10,12 +10,12 @@ end
 
 puts "what would you do with a million pounds?"
 
-options = {"New House"=> 650,"Red Ferrari" => 225, "Buy Tech-Firm" =>150, "Worldwide Cruise" => 75, 
-"Pay Off Mortgage" => 250, "Visit Las Vegas" => 100, "Six Months No-Work" => 50}
+options = {"New House"=> 650,"Red Ferrari" => 225, "Buy Tech Firm" =>150, "Worldwide Cruise" => 75, 
+"Pay Off Mortgage" => 250, "Visit Las Vegas" => 100, "Six Months No Work" => 50}
 
 puts
 
-#show user list of options
+
 
 begin
   puts "type 'opt' for options"
@@ -26,7 +26,7 @@ puts "Your choices are:"
 puts
 display_menu(options)
 puts
-puts "What will you spend on your million on?"
+puts "Pick from the list above"
 
 
 balance = 1000
@@ -39,7 +39,7 @@ begin
 	if  options.has_key?(user_choice)
 		balance = balance - options[user_choice]
 		display_balance (balance)
-		puts "keep spending ?"
+		
 		
 	else 
 		puts "<invalid choice>"
@@ -47,9 +47,19 @@ begin
 		display_menu(options)
 		puts "only above list (case sensitive)"
 	end
+	
+	if 	options.has_key?(user_choice) && balance >=0
+		puts "Go on, spend some more"
+	end
+
+	if 	balance <0
+		puts "You've spent too much; you need to sell something"
+	end
 
 
 end while balance >0
+
+
 
 #IDEAS TO ENHANCE GAME
 #IF USER DOES NOT WANT TO USE ALL THEY CAN GIVE TO CHARITY OF THEIR CHOICE
