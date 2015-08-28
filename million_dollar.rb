8a30abdf13c901641aad29420c993fc0bb44e108
lmdj07 <lmdj07@gmail.com>
2015-08-28 22:46:05 +0100

    various options for charity

--- million_dollar.rb
+++ million_dollar.rb
@@ -1,5 +1,5 @@
 def display_balance(balance)
-  puts "Your balance is #{balance}"
+  puts "Your balance is #{@balance}"
 end
 
 def display_menu(menu)
@@ -12,15 +12,27 @@ end
 
 def show_charities (charity)
 	charity.each { |key, value| puts "#{value}"}
-
 end
 
+def decision_charities(give_to)
+	user_choice = gets.chomp
+	if @charity_list.has_value?(user_choice)
+		puts"enter amount to give"
+		puts
+		puts "you have #{@balance} left"
+		puts
+		amount_to_give = gets.chomp.to_i
+		@balance = @balance - amount_to_give
+		puts "you have #{@balance} left"
+	else puts "not in list"
+	end
+end
 puts "what would you do with a million pounds?"
 
 options = {"New House"=> 650,"Red Ferrari" => 225, "Buy Tech Firm" =>150, "Worldwide Cruise" => 75, 
 "Pay Off Mortgage" => 250, "Visit Las Vegas" => 100, "Six Months No Work" => 50}
 
-charity_list = {1=>"Save the Children", 2=>"Cancer Research UK", 3=>"Aged UK", 4=>"NSPCC", 5=>"Centre Point", 6=>"Shelter", 7=>"Salvation Army", 8=>"The Disabilities Trust"}
+@charity_list = {1=>"Save the Children", 2=>"Cancer Research UK", 3=>"Aged UK", 4=>"NSPCC", 5=>"Centre Point", 6=>"Shelter", 7=>"Salvation Army", 8=>"The Disabilities Trust"}
 puts
 
 
@@ -36,7 +48,7 @@ puts
 puts "Pick from the list above"
 
 
-balance = 1000
+@balance = 1000
 
 begin 
 
@@ -45,8 +57,8 @@ begin
 
 
 	if  options.has_key?(user_choice)
-		balance = balance - options[user_choice]
-		display_balance (balance)
+		@balance = @balance - options[user_choice]
+		display_balance (@balance)
 
 	elsif 
 		user_choice == "Give to Charity"
@@ -54,8 +66,11 @@ begin
 		puts "Our preferred Charities List"
 		puts
 		puts "**********************"
-		show_charities(charity_list)
+		show_charities(@charity_list)
 		puts "**********************"
+		puts
+		puts "Select from above list or let us choose for you"
+		decision_charities(user_choice)
 
 	else 
 		puts "<invalid choice>"
@@ -64,24 +79,25 @@ begin
 		puts "only above list (case sensitive)"
 	end
 	
-	if 	options.has_key?(user_choice) && balance >=0
-		puts "Go on, spend some more"
+	if 	options.has_key?(user_choice) && @balance >=0
+		puts "Go on, spend some more or Give to Charity"
 	end
 
-	if 	balance <0
+	if 	@balance <0
 		puts "You've spent too much; you need to sell something"
 	end
 
 
-end while balance >0
+end while @balance >0
 
 
 
-#IDEAS TO ENHANCE GAME
-#IF USER DOES NOT WANT TO USE ALL THEY CAN GIVE TO CHARITY OF THEIR CHOICE
-###need to display a list of charities
-#IF USER GOES OVER A MILLION, THEY NEED TO SELL BACK TO THE GAME AT A 5% LOSS
-#if user chooses to start all over again, they get 10% less money (maybe 10% of their ending balance deducted from  the million)
-#if the user choses to abort the game altogether, they must leave a comment; comments need to be saved to a log file
-#if user wants something not in the list, they can say what that is and fetch the price from ???? web?
-
+# IDEAS TO ENHANCE GAME
+######################################################################################################
+# If user does not want to spend all they can give to charity from list of preferred chrities - DONE
+# need to display a list of charities - DONE
+# If user goes over a millios they need to sell back at a 5% LOSS
+# if user chooses to start all over again, they get 10% less money (maybe 10% of their ending balance 
+# deducted from  the million)
+# if the user choses to abort the game altogether, they must leave a comment; comments need to be saved to a log file
+# if user wants something not in the list, they can say what that is and fetch the price from ???? web?
